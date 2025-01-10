e707aef752c3bb03629e4346448785157f8b4a23
Secondarian <robertanitei@tutanota.com>
2025-01-11 00:39:32 +0200

    e1s: Initial haptics setup

--- device.mk
+++ device.mk
@@ -79,6 +79,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-latest.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Haptics
+PRODUCT_PACKAGES += vibrator.default
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.example \
